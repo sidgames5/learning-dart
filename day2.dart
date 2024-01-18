4bfdff53133bbd288262664f7d6d8b6b81f9f7b5
SidGames5 <saytleader@outlook.com>
2024-01-18 16:39:57 -0500

    day 2

---
+++ day2.dart
@@ -0,0 +1,22 @@
+// left off at 15:44
+
+import "dart:io";
+
+void main() {
+  int amount = 10;
+  print(amount);
+
+  double gpa = 3.9;
+  print(gpa);
+
+  String name = "Sid";
+  print(name);
+
+  bool passing = true;
+  print(passing);
+
+  dynamic something = 10;
+  print(something);
+  something = "hi";
+  print(something);
+}
